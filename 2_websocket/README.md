1c0677dd2d765417e68f85963f57f36a4c5ac6a2
TLH <tiagolorahansen@gmail.com>
2024-05-06 17:15:18 -0300

    Add: websocket exercise implementation

---
+++ 2_websocket/README.md
@@ -0,0 +1,12 @@
+# Objetivo
+Criar, a partir do código fornecido em *WebSocket*, um servidor capaz de receber dois números, somar, e apresentar no *browser* a resposta.
+
+# Inicialização
+
+1. Em um terminal, entre na pasta "/2_websocket" e execute:
+
+`python3 server.py`
+
+2. Copie o caminho (path) absoluto do arquivo "/2_websocket/client.html", cole no navegador e pressione enter. Alternativamente, abra a pasta na qual o arquivo está localizado pelo explorador de arquivos e dê um duplo clique em "client.html".
+
+3. Siga as instruções dos *outputs* do terminal e observe as somas serem exibidas na tela.
---
+++ 2_websocket/server.py
@@ -0,0 +1,18 @@
+import asyncio
+import random
+import websockets
+
+async def time(websocket, path):
+    while True:
+        try:
+            first_number = int(input("Primeiro número: "))
+            second_number = int(input("Segundo número: "))
+        except ValueError:
+            print("Digite apenas números")
+            continue
+        await websocket.send(f"{first_number} + {second_number} = {first_number + second_number}")
+        await asyncio.sleep(random.random() * 3)
+
+start_server = websockets.serve(time, "127.0.0.1", 5678)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
